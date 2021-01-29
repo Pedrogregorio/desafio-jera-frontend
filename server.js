62c371e25e12c116c420aba09ea8da11239cd648
Pedrogregorio <pedrogregoriodias675@gmail.com>
2021-01-29 16:06:40 -0400

    Erro de rotas resolvido

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const serverStatic = require('serve-static')
+const express = require('express');
+const history = require('connect-history-api-fallback');
 const path = require('path')
+const app = express();
 
-const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, 'dist'));
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 
-const port = process.env.PORT || 8080
-app.listen(port)
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
